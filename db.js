1f53a1713f5257e8623d147db0ccbc0f64fbe7d6
Megan Neyer <Megan.Neyer@gmail.com>
2021-03-06 13:50:58 -0500

    post-heroku deployment

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 // require("dotenv").config();
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize('beast-mode-server', 'postgres', 'password', {
-//     host: 'localhost',
-//     dialect: 'postgres'
-// });
+const sequelize = new Sequelize('beast-mode-server', 'postgres', 'password', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
 
 //if you want to run locally, comment out 10-15 (dialect) or use lines 2-5 instead of the below.
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // very important
-        }
-    }
-})
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false, // very important
+//         }
+//     }
+// })
 
 
 sequelize.authenticate().then(
